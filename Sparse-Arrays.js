1ca2083d3878346062fd7d864026719c3d2af3f4
Yen Pinero Ferro <yenf89@gmail.com>
2024-07-09 00:08:56 -0400

    Sparse-Arrays

--- Sparse-Arrays.js
+++ Sparse-Arrays.js
@@ -1,9 +1,7 @@
 /*There is a collection of input strings and a collection of query strings.
- For each query string, determine how many times it occurs in the list of input strings. 
+ For each query string, determine how many times it occurs in the list of input strings.
  Return an array of the results.  */
-
 function matchingStrings(strings, queries) {
-    // Step 1: Create a frequency dictionary for the input strings
     var frequencyDict = {};
     for (var _i = 0, strings_1 = strings; _i < strings_1.length; _i++) {
         var str = strings_1[_i];
@@ -14,7 +12,6 @@ function matchingStrings(strings, queries) {
             frequencyDict[str] = 1;
         }
     }
-    // Step 2: Query the frequency dictionary
     var result = [];
     for (var _a = 0, queries_1 = queries; _a < queries_1.length; _a++) {
         var query = queries_1[_a];
--- Sparse-Arrays.ts
+++ Sparse-Arrays.ts
@@ -1,9 +1,8 @@
 /*There is a collection of input strings and a collection of query strings.
  For each query string, determine how many times it occurs in the list of input strings. 
  Return an array of the results.  */
- 
+
 function matchingStrings(strings: string[], queries: string[]): number[] {
-    // Step 1: Create a frequency dictionary for the input strings
     const frequencyDict: { [key: string]: number } = {};
     
     for (const str of strings) {
@@ -14,7 +13,6 @@ function matchingStrings(strings: string[], queries: string[]): number[] {
         }
     }
 
-    // Step 2: Query the frequency dictionary
     const result: number[] = [];
     
     for (const query of queries) {
